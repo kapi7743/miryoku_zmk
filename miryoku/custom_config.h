e62fed64caf947bb2dab550e96da294cc77f7e5b
kapi7743 <kacper.kolbuc@gmail.com>
2024-07-26 18:14:25 +0200

    first config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_COLEMAKDH
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEKAKDH
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
+
