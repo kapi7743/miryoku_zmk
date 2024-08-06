0bf5154d89cd445eb92c853b730bf4ba8a750cbd
kapi7743 <120283755+kapi7743@users.noreply.github.com>
2024-08-07 01:36:08 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,10 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-#define MIRYOKU_ALPHAS_COLEMAKDH
-#define MIRYOKU_TAP_QWERTY
-#define MIRYOKU_EXTRA_COLEKAKDH
-#define MIRYOKU_NAV_VI
-#define MIRYOKU_CLIPBOARD_WIN
-#define MIRYOKU_MAPPING_EXTENDED_THUMBS
-
